088477cf8fd77c7a02a67e460024cf1de8e043ed
Huy <huyp7k8145@gmail.com>
2022-01-02 23:18:54 +0700

    thích gì ghi nấy :))

---
+++ action_page.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+Hello <?php echo $_GET["Name"]; ?>
+Your Email is <?php echo $_GET["Email"]; ?> 
+Your Message is <?php echo $_GET["Message"]; ?><br>
+
+</body>
+</html>
